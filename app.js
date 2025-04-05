0e6ee738ef4d1e06c5f6bd800bce68cc0c8ce37f
Tyler Cozzocrea <tylercozzocrea@gmail.com>
2025-04-05 15:57:39 -0700

    map centers at corvallis

--- app.js
+++ app.js
@@ -3,10 +3,33 @@ console.log("Hello, Node.js!");
 function initMap() {
   // Map options
   const options = {
-    zoom: 8,
-    center: { lat: 40.7128, lng: -74.006 }, // New York coordinates
+    zoom: 15,
+    center: { lat: 44.5648, lng: -123.276 }, // Corvallis Cords
   };
 
   // New map
   const map = new google.maps.Map(document.getElementById("map"), options);
+
+  // Get current location
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        map.setCenter(pos);
+
+        // Add marker for current location
+        new google.maps.Marker({
+          position: pos,
+          map: map,
+          title: "Current Location",
+        });
+      },
+      () => {
+        // TODO Do Something
+      },
+    );
+  }
 }
